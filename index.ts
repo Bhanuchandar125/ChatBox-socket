38651f081a710e51ac33c2d9554d21a5e75daf2a
Bhanu <bcr.chilukoti@gmail.com>
2023-10-05 19:36:40 +0530

    'onlineUsers'

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
 const { Server } = require("socket.io");
 
-const io = new Server({cors: "http://localhost:5174"});
+const io = new Server({cors: "http://localhost:5173"});
 
-let onlineUser=[]
-io.on("connection", (socket) => {
+let onlineUser:any=[]
+io.on("connection", (socket:any) => {
   console.log("new Connection", socket.id);
 
   //listen to connection
-  socket.on("addNewUser", (userId)=>{
+  socket.on("addNewUser", (userId:any)=>{
     console.log("userId", userId)
-    !onlineUser.some((user)=>user.userId === userId)&&
+    !onlineUser.some((user:any)=>user.userId === userId)&&
     onlineUser.push({
       userId:userId,
       socketId: socket.id,
@@ -17,7 +17,10 @@ io.on("connection", (socket) => {
     console.log("onlineUsers", onlineUser)
     io.emit("getOnlineUsers", onlineUser)
   });
-  
+  socket.on("disconnect", ()=>{
+    onlineUser= onlineUser.filter((user:any)=>user?.socketId!== socket.id);
+    io.emit("getOnlineUsers", onlineUser)
+  })
 });
 
 io.listen(3000);
